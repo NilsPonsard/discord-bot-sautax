832b79777bdaf7ae4bafb7b3bbadbffb2faa8da5
sautax <sautaxgaming@gmail.com>
2019-08-18 00:33:51 +0200

    fix

--- image.py
+++ image.py
@@ -10,18 +10,18 @@ async def mandelbrot(n=10):
     start = time.time()
     max_size = 500
     imageArray = []
-    ratio = 2.1/(max_size-1)
+    sizeone = (max_size-1)
     ratio_i = int(255/n)
     pre_z = complex(0, 0)
     for y in range(max_size):
         for x in range(max_size):
             z = pre_z
-            c = complex(x*ratio-1.5, y*ratio-1)
+            c = complex(2.1*x/sizeone-1.5, 2.1*y/sizeone-1)
             i = 0
-            while (i < n) and z.real*z.imag < 4:
+            while (i < n) and abs(z) < 4:
                 i += 1
                 z = z*z+c
-            level = i*ratio_i
+            level = int(i*ratio_i)
             imageArray.append((level, level, level))
     checkpoint = time.time() - start
     image = PIL.Image.new("RGB", (max_size, max_size))
