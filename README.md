d8febe09ddb960b4527d7beabe59e02dd71c5fa5
sautax <sautaxgaming@gmail.com>
2019-08-22 00:34:29 +0200

    updates to the readme and the daemon

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-sautax-bot
+# sautax-bot
+
+A discord bot written in python
+
+## setup
+
+- place your discord bot token in the `secret` file
+- place your riot api key in the `lolKey` file
+- place the discord user ID of the admin in the `trustedIDs` file
+- compile the [mandelbrot-rust](https://github.com/sautax/mandelbrot-rust), rename it `mandelbrot` and place it in the mandelbrot folder
+- have python (>3.5.3) installed on the machine with the `discord.py` framework installed (with [pip](https://pypi.org/project/discord.py/))
+
+## running the bot
+
+You can start the bot by running the `daemon.py` if you want to use the reload functionnality or you can direcly launch `serveur.py`
--- daemon.py
+++ daemon.py
@@ -2,9 +2,22 @@
 from subprocess import Popen
 import sys
 
+python_executable = "python3"
+filename = " serveur.py"
+found_python = True
+try:
+    p = Popen(python_executable + filename, shell=True)
+    p.wait()
+except:
+    python_executable = "python"
+    try:
+        p = Popen(python_executable + filename, shell=True)
+        p.wait()
+    except:
+        print("Unable to find a python executable with discord.py installed you may change the python_executable variable in the daemon.py script")
+        found_python = False
 
-filename = "serveur.py"
-while True:
+while found_python:
     print("\nStarting " + filename)
-    p = Popen("python3 " + filename, shell=True)
+    p = Popen(python_executable + filename, shell=True)
     p.wait()
